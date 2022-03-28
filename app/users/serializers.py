55cf004dfe605ac1840c81d1c1ec6594d0fe6743
hamed1361554 <hamed_z554@yahoo.com>
2022-03-28 21:01:07 +0430

    User Managements

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -16,6 +16,18 @@ class UserSerializer(ModelSerializer):
 
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Updates user by validated data"""
+
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(Serializer):
     """Auth Token Serializer"""
--- app/users/tests/test_user_api.py
+++ app/users/tests/test_user_api.py
@@ -3,10 +3,11 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 import rest_framework.status as status
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 
 
 CREATE_USERS_URL = reverse('users:create')
+SELF_USER_URL = reverse('users:me')
 
 
 class PublicUserApiTests(TestCase):
@@ -58,3 +59,52 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(get_user_model().objects.filter(email=payload['email']).exists())
+
+    def test_unauthorized_user_retrieval(self):
+        """Tests that user retrieval needs authorization"""
+        res = self.client.post(SELF_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(APITestCase):
+    """Private User Api Tests"""
+
+    def setUp(self):
+        """Sets up"""
+
+        self.user = get_user_model().objects.create_user(
+            email='test@tosan.com',
+            password='testpassword',
+            name='testuser'
+        )
+
+        #self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_authorized_user_retrieval(self):
+        """Tests that user retrieval needs authorization"""
+
+        res = self.client.get(SELF_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'id': res.data.get('id'),
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_self_user_invalid_data(self):
+        """Tests than null payload is invalid"""
+
+        res = self.client.post(SELF_USER_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_user_update_successful(self):
+        """Tests that user update is successful"""
+
+        payload = {'name': 'new name', 'password': 'newpassworduser'}
+        res = self.client.patch(SELF_USER_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data['email'], self.user.email)
+        self.assertTrue(self.user.check_password(payload['password']))
